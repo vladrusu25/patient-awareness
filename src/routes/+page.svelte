c8a39743f7ae3bd4ba3fdef574b10bb30296a0f7
Vlad Rusu <v.rusu@student.maastrichtuniversity.nl>
2025-08-26 20:03:48 +0300

    Normalize line endings to LF (Windows scripts CRLF)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<input class="mt-6 block w-full rounded-lg border p-2" placeholder="Hello Tailwind" />
